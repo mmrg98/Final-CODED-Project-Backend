475b7f3fc09d4e9af72c9bca3dd1559e70e6a439
hanodims <hend--mohammed@outlook.com>
2020-11-17 19:30:29 +0300

    Models & Admin Page

--- fammunity/admin.py
+++ fammunity/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import Profile,Follower,Post,Photo,Item,Comment,Like
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ['user', 'gender']
+    list_display = ['user', 'gender']
+    list_filter = ['gender']
+    search_fields = ['user',]
+    list_display_links = ['user',]
+
+admin.site.register(Profile,ProfileAdmin)
+
+
+class FollowerAdmin(admin.ModelAdmin):
+    fields = ['user', 'follower']
+    list_display = ['user', 'follower']
+    list_filter = ['user', 'follower']
+    list_display_links = ['user',]
+
+admin.site.register(Follower,FollowerAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ['owner', 'created','modified','description']
+    list_display = ['owner', 'created','modified','description']
+    list_filter = ['owner', 'created','modified']
+    search_fields = ['description',]
+    list_display_links = ['owner',]
+
+admin.site.register(Post,PostAdmin)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    fields = ['post', 'image']
+    list_display = ['post', 'image']
+    list_display_links = ['post',]
+
+admin.site.register(Photo,PhotoAdmin)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    fields = ['name', 'brand','store','size','price','post']
+    list_display = ['name', 'brand','store','size','price','post']
+    list_filter = ['brand','store','size','price']
+    search_fields = ['name', 'brand','store','size','price','post']
+    list_display_links = ['name',]
+
+admin.site.register(Item,ItemAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = ['txt', 'commenter','post']
+    list_display = ['txt', 'commenter','post']
+    list_filter = ['commenter','post']
+    search_fields = ['txt', 'commenter','post']
+    list_display_links = ['commenter',]
+
+admin.site.register(Comment,CommentAdmin)
+
+
+class LikeAdmin(admin.ModelAdmin):
+    fields = ['fan', 'post']
+    list_display = ['fan', 'post']
+    list_filter = ['fan']
+    search_fields = ['post']
+    list_display_links = ['fan',]
+
+admin.site.register(Like,LikeAdmin)
--- fammunity/models.py
+++ fammunity/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
 	)
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	gender = models.CharField(choices=GENDER, max_length=2, null=True)
-	image = models.ImageField(upload_to='profile_pic', default='profile.png')
+	image = models.ImageField(upload_to='profile_image', default='profile.png')
 
 	def __str__(self):
 		return self.user.username
@@ -22,3 +22,56 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user= instance)
+
+
+class Follower(models.Model): 
+	user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+	follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+	def __str__(self):
+		return '%s' % (self.user.user.username)
+		 
+
+
+class Post(models.Model):
+	description = models.TextField(max_length=500)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
+	owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+	def __str__(self):
+		return '%s ,id: %s' % (self.owner.user.username, self.id)
+
+
+
+class Photo(models.Model): 
+	image = models.ImageField(upload_to='post_photos')
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='photos')
+
+	def __str__(self):
+		return '%s' % (self.post.owner.user.username)
+
+
+
+class Item(models.Model):
+	name = models.CharField(max_length=250) 
+	brand = models.CharField(max_length=250,blank=True,null=True) 
+	store = models.CharField(max_length=250,blank=True,null=True) 
+	size = models.IntegerField(blank=True,null=True) 
+	price = models.DecimalField(max_digits=4, decimal_places=2,blank=True,null=True)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='items')
+	
+	def __str__(self):
+		return '%s:  %s' % (self.post.owner.user.username, self.name)
+
+
+
+class Comment(models.Model):
+	txt = models.TextField(max_length=300)
+	commenter = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	
+
+class Like(models.Model): 
+	fan = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='fans')
