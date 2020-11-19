31aee8e6673a7a8f7cd49fe7d78ac5c86a9069ca
mmrg98 <mariam.elgaria98@gmail.com>
2020-11-19 13:50:43 +0300

    add create comment

--- fammunity/models.py
+++ fammunity/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
 		("F", "Female"),
 		("M", "Male")
 	)
-	followers = models.ManyToManyField("self", related_name="following")
+	followers = models.ManyToManyField("self", related_name="following", blank=True, null=True)
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	gender = models.CharField(choices=GENDER, max_length=2, null=True)
 	image = models.ImageField(upload_to='profile_image', default='profile.png')
@@ -25,11 +25,8 @@ def create_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user= instance)
 
 
-
-
-
 class Post(models.Model):
-	liked_by = models.ManyToManyField(Profile, related_name='likes')
+	liked_by = models.ManyToManyField(Profile, related_name='likes', blank=True, null=True)
 	description = models.TextField(max_length=500)
 	created = models.DateTimeField(auto_now_add=True)
 	modified = models.DateTimeField(auto_now=True)
@@ -40,7 +37,7 @@ class Post(models.Model):
 
 
 
-class Photo(models.Model): 
+class Photo(models.Model):
 	image = models.ImageField(upload_to='post_photos')
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='photos')
 
@@ -50,13 +47,13 @@ class Photo(models.Model):
 
 
 class Item(models.Model):
-	name = models.CharField(max_length=250) 
-	brand = models.CharField(max_length=250,blank=True,null=True) #separate model 
-	store = models.CharField(max_length=250,blank=True,null=True) #separate model 
-	size = models.IntegerField(blank=True,null=True) 
+	name = models.CharField(max_length=250)
+	brand = models.CharField(max_length=250,blank=True,null=True) #separate model
+	store = models.CharField(max_length=250,blank=True,null=True) #separate model
+	size = models.IntegerField(blank=True,null=True)
 	price = models.DecimalField(max_digits=4, decimal_places=2,blank=True,null=True)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='items')
-	
+
 	def __str__(self):
 		return '%s:  %s' % (self.post.owner.user.username, self.name)
 
@@ -66,4 +63,3 @@ class Comment(models.Model):
 	txt = models.TextField(max_length=300)
 	commenter = models.ForeignKey(Profile, on_delete=models.CASCADE)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-	
